14256b2211e691239977e3259854793a1206cef9
Karla Torres <ktiktok96@gmail.com>
2022-03-19 22:06:28 -0400

    Work on BestLoanRates Class
    Created a New BestLoanRatesClass. TODO: get the username using Scanner API

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,13 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+    private static int add(int[] numbers){
+        int sum = 0;
+        for(int item: numbers ){
+            sum += item;
+        }
+        return sum;
     }
 }
